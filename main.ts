c59b701a480f4d5cfd269255d051e0b60d5e9334
awtnb <arikaebanataw@gmail.com>
2024-11-01 13:48:47 +0900

    fix

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ import {
 
 const withSuffix = (path: string, suffix: string): string => {
   const parts = path.split(".");
-  const extension = parts.pop() || "";
+  const extension = parts.pop() || "pdf";
   return parts.join(".") + suffix + "." + extension;
 };
 
